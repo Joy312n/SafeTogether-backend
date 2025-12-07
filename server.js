1bdaaf87efba3fd80f80d4fa837037a76237191c
Joy132n <nitaichanda2@gmail.com>
2025-12-07 21:13:09 +0530

    localhostapi

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ connectDB();
 
 const app = express();
 app.use(cors({
-  origin: ["http://localhost:5173", "https://safe-together-eta.vercel.app"], // Add your frontend URLs
+  origin: ["http://localhost:5173","http://localhost:5174","http://localhost:5175", "https://safe-together-eta.vercel.app"], // Add your frontend URLs
   credentials: true
 }));
 app.use(express.json());
